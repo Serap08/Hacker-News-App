ad87be681d28835d5bbe13d3587be342552584c9
Serap <s.qamar08@yahoo.com>
2018-11-14 12:45:29 -0800

    Adding UI for Stories, Integrating responsiveness for smaller screens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { StoriesComponent } from './stories/stories.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    StoriesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
